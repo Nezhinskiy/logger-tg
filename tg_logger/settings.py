531722ea15fe64c53c69bac02d42421192fbd5fa
Mikhail <103121580+Nezhinskiy@users.noreply.github.com>
2023-10-30 21:46:25 +0100

    Minor changes

--- tg_logger/settings.py
+++ tg_logger/settings.py
@@ -12,4 +12,3 @@ class SyncTgLoggerSettings:
 def configure_logger(bot_token: str, recipient_id: int):
     global logger_settings
     logger_settings = SyncTgLoggerSettings(bot_token, recipient_id)
-
