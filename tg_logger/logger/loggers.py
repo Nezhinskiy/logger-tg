76967b74c08ea560fc7edabc9c70e27bac391d1e
Mikhail <103121580+Nezhinskiy@users.noreply.github.com>
2024-02-24 20:04:48 +0300

    Enhanced Logging Flexibility and Configuration Options

--- tg_logger/logger/loggers.py
+++ tg_logger/logger/loggers.py
@@ -1,5 +1,7 @@
 import logging
+import sys
 from datetime import datetime, timezone
+from logging.handlers import TimedRotatingFileHandler
 
 from tg_logger.logger.logger_warner import ClientLogger
 from tg_logger.settings import SyncTgLoggerSettings
@@ -8,15 +10,55 @@ from tg_logger.utils import model_dict_or_none
 
 class BaseLogger:
     shrug: str = ''.join(map(chr, (175, 92, 95, 40, 12484, 41, 95, 47, 175)))
+    FORMATTER: logging.Formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s'
+    )
 
     def __init__(
             self,
             bot_token: str = None,
             recipient_id: int = None,
+            name: str = 'app',
     ) -> None:
-        from tg_logger.settings import logger_settings
-        self.logger = logging.getLogger('app')
+        self.logger = logging.getLogger(name)
         self.start_time = datetime.now(timezone.utc)
+        self.tg_logger = self.configure_tg_logger(bot_token, recipient_id)
+
+    def __getattr__(self, name: str) -> callable:
+        if name.startswith('__') and name.endswith('__'):
+            return super().__getattr__(name)
+        return lambda *args, **kwargs: self.log_message(name, *args, **kwargs)
+
+    @staticmethod
+    def get_console_log_handler(
+            formatter: logging.Formatter = FORMATTER,
+            level: int = logging.DEBUG
+    ) -> logging.StreamHandler:
+        console_log_handler = logging.StreamHandler(sys.stderr)
+        console_log_handler.setFormatter(formatter)
+        console_log_handler.setLevel(level)
+        return console_log_handler
+
+    @staticmethod
+    def get_file_log_handler(
+            path: str,
+            formatter: logging.Formatter = FORMATTER,
+            level: int = logging.INFO,
+            **kwargs
+    ) -> TimedRotatingFileHandler:
+        file_log_handler = TimedRotatingFileHandler(
+            filename=kwargs.pop('filename', path), **kwargs
+        )
+        file_log_handler.setFormatter(formatter)
+        file_log_handler.setLevel(level)
+        return file_log_handler
+
+    def configure_tg_logger(
+            self,
+            bot_token: str,
+            recipient_id: int
+    ) -> ClientLogger:
+        from tg_logger.settings import logger_settings
         try:
             if bot_token is None:
                 bot_token = logger_settings.bot_token
@@ -29,12 +71,24 @@ class BaseLogger:
             ) from AttributeError
         else:
             settings = SyncTgLoggerSettings(bot_token, recipient_id)
-            self.tg_logger = ClientLogger(settings, self.logger)
+            return ClientLogger(settings, self.logger)
 
-    def __getattr__(self, name):
-        if name.startswith('__') and name.endswith('__'):
-            return super().__getattr__(name)
-        return lambda *args, **kwargs: self.log_message(name, *args, **kwargs)
+    def get_logger(
+            self,
+            name: str = None,
+            level: int = None,
+            console_log_handler: logging.StreamHandler = None,
+            file_log_handler: TimedRotatingFileHandler = None
+    ) -> 'BaseLogger':
+        logger = logging.getLogger(name)
+        if console_log_handler is not None:
+            logger.addHandler(console_log_handler)
+        if file_log_handler is not None:
+            logger.addHandler(file_log_handler)
+        if level is not None:
+            logger.setLevel(level)
+        self.logger = logger
+        return self
 
     def error(self, message) -> None:
         self.tg_logger.send_error(message)
